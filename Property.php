300c378f77f1aec55c7418e5dc3147a29443430e
iqbal558 <77089014+iqbal558@users.noreply.github.com>
2021-01-07 14:55:57 +0700

    Create Property.php

---
+++ Property.php
@@ -0,0 +1,9 @@
+<?php
+//cara penulisan class dan property OOP PHP
+class mobil {                     //class mobil
+     var $warna;                  //class warna
+     var $merek;                  //class merek
+     var $ukuran;                 //class ukuran
+
+}
+?>
