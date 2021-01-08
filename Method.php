1d5cd7b7b006c1c2047d3d1b52c8a6944432ad24
iqbal558 <77089014+iqbal558@users.noreply.github.com>
2021-01-08 13:46:28 +0700

    Create Method.php

---
+++ Method.php
@@ -0,0 +1,21 @@
+<?php
+/ / cara penulisan class dan property OOP PHP - 
+class motor {
+       / /  property OOP 
+       var $ purple ;
+       var $ Scopy;
+       var $ 20 ;
+
+        / / method OOP 
+        function maju ( ) {
+        motor saya berjalan maju
+          
+       / / isi method 
+}
+        function berhenti ( ) {
+       motor saya berhenti bergerak 
+         }
+
+}
+
+?>
